9fd3853d4841d6040760603c24aa1e46ae0330b1
RaParedez <robertoparedesmm@gmail.com>
2025-03-18 19:22:41 -0600

    agrega amigos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+var listaDeNombres = [];
+
+
+function agregarAmigo() {
+  let nombre = document.getElementById("amigo").value;
+  if ( nombre !== "" ){
+    listaDeNombres.push(nombre);
+    document.getElementById("amigo").value = "";
+    document.getElementById("amigo").focus();
+    console.log(listaDeNombres);
+  } else {
+    alert("Ingrese un nombre");
+  }
+}
+
+function sortearAmigo(){
+
+}
+
